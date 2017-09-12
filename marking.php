4dcec9bc06c5a5277e2c0a57b4c48914af7a77a7
Dan McKeown <dangmckeown@users.noreply.github.com>
2017-09-12 11:52:07 +0100

    Oops, included quiz bit

--- marking.php
+++ marking.php
@@ -17,21 +17,6 @@
 
 <div style="font-size: 14pt; font-family: 'Arial Narrow'">
 
-  <p style="font-weight: bold;">Quiz, you scurvy apes</p>
-  
-<?php
-
-include_once('functions.php');
-
-short_quiz(5);
-
-?>
-<p>Find out <a href="index.php">what happened on this day in history</a>.</p>
-</div>
-</html>
-
-<div style="font-size: 14pt; font-family: 'Arial Narrow'">
-
   <p style="font-weight: bold;">Answers</p>
 
 <?php
