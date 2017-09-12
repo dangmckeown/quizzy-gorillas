76d65f18c9f662046b0725074586e6b2da5f325d
Dan McKeown <dangmckeown@users.noreply.github.com>
2017-09-12 11:48:28 +0100

    Improve formatting for mobile with html wrapper

--- quiz.php
+++ quiz.php
@@ -1,8 +1,25 @@
+
+<!doctype html>
+<html>
+
+	<head>
+		<title>Quizzy Gorillas - General knowledge Quiz</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<style>
+			body {font-family: 'Arial Narrow';} 
+			li {font-size: 14pt;}
+			li { padding-bottom: 3px; }
+			h1 {font-size: 18pt; font-weight: bold;}
+			h2 {font-size: 16pt; font-weight: normal;}
+		</style>
+	</head>
+	<body>
+
 <div style="font-size: 14pt; font-family: 'Arial Narrow'">
 
   <p style="font-weight: bold;">Quiz, you scurvy apes</p>
   
-<?
+<?php
 
 include_once('functions.php');
 
@@ -11,3 +28,4 @@ short_quiz(5);
 ?>
 <p>Find out <a href="index.php">what happened on this day in history</a>.</p>
 </div>
+</html>
