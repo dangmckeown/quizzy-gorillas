464355933303310237cb3b0177631c21e43b9e4c
Dan McKeown <dangmckeown@users.noreply.github.com>
2017-09-12 11:49:29 +0100

    Add html wrapper for better formatting

--- marking.php
+++ marking.php
@@ -1,15 +1,45 @@
+
+<!doctype html>
+<html>
+
+	<head>
+		<title>Quizzy Gorillas - Quiz Answers</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<style>
+			body {font-family: 'Arial Narrow';} 
+			li {font-size: 14pt;}
+			li { padding-bottom: 3px; }
+			h1 {font-size: 18pt; font-weight: bold;}
+			h2 {font-size: 16pt; font-weight: normal;}
+		</style>
+	</head>
+	<body>
+
+<div style="font-size: 14pt; font-family: 'Arial Narrow'">
+
+  <p style="font-weight: bold;">Quiz, you scurvy apes</p>
+  
+<?php
+
+include_once('functions.php');
+
+short_quiz(5);
+
+?>
+<p>Find out <a href="index.php">what happened on this day in history</a>.</p>
+</div>
+</html>
+
 <div style="font-size: 14pt; font-family: 'Arial Narrow'">
 
   <p style="font-weight: bold;">Answers</p>
 
-<?
+<?php
 
 include_once('questions.php');
 
 $total_score = 0;
 $possible_score = $_POST['possible_score'];
-
-#var_dump($_POST);
 
 foreach ($_POST as $q => $ans){
 
@@ -42,3 +72,4 @@ echo "<p><a href=\"quiz.php\">Give me another go</a></p>";
 <p>Find out <a href="index.php">what happened on this day in history</a>.</p>
 
 </div>
+</html>
