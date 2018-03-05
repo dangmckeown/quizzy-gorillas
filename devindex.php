07b569842af2a865de02a13c8ed9351ff6994d8a
Dan McKeown <dangmckeown@users.noreply.github.com>
2018-03-05 22:34:16 +0000

    Use rollback devindex code

--- devindex.php
+++ devindex.php
@@ -22,45 +22,26 @@
 		
 	
 <?php
-
 $day = date('j');
-
 $month = date('F');
-
 echo "<h1>$day $month</h1>";
-
 $wikis = file_get_contents('https://en.wikipedia.org/wiki/' . $month . '_' . $day);
-
 $bbcs = file_get_contents('http://news.bbc.co.uk/onthisday/hi/dates/stories/' . strtolower($month) .'/' . $day .'/default.stm');
-
 //== Trim bbc output
-
 $bbc_trim=array();
-
 $bbs = explode("<td>", $bbcs);
-
 $regex = "/\/onthisday\/hi\/dates\/stories\/" . strtolower($month) ."\/" . $day . ".*\d\.stm/";
-
 foreach ($bbs as $bb){
-
 if (preg_match($regex,$bb)){
 $bbc_trim[] = trim(str_replace(":","",strip_tags($bb)));
 } //end if preg_match
-
 } //end foreach bbs
-
 //== Get Beautiful Britain after site rejig
-
 $brit_base = file_get_contents("http://www.beautifulbritain.co.uk/htm/onthisday/onthisday.htm");
-
 $today_url = array();
-
 $brits = file_get_contents('http://www.beautifulbritain.co.uk/htm/onthisday/current/'.$day.'.htm');
-
 $preg_url = "/".$month."\/.*".$day."\.htm/";
-
 preg_match($preg_url, $brit_base, $today_url);
-
 if(! $today_url[0]){
 echo "ERROR getting Beautiful Britain URL! <a href='http://www.beautifulbritain.co.uk/htm/onthisday/onthisday.htm'>Go to the site</a>";
 }
@@ -68,55 +49,42 @@ else
 {
 $brits = file_get_contents('http://www.beautifulbritain.co.uk/htm/onthisday/' . $today_url[0]);
 }
-
 //=== Trim Wiki Output
-
 $wiks = explode("<h2>Contents</h2>", $wikis);
-
 $wis = explode("<span class=\"mw-headline\" id=\"Events\">Events</span>",$wiks[1]);
-
 $ws = explode("<span class=\"mw-headline\" id=\"Births\">Births</span>",$wis[1]);
-
 $wiki_trim = explode("<li>",str_replace(" – "," ", $ws[0]));
 foreach ($wiki_trim as $trim){
 	$trim = strip_tags($trim);
 }
-
 //== Trim Beautiful Britain Output
-
 $brit_trim = explode("<p>",$brits);
-
+		
+	
 //== Consolidate and order sources
-
 $sources = ([$bbc_trim, $wiki_trim, $brit_trim]);
-
 foreach ($sources as $source){
+print_r($source);
+	echo "<p />";
+}		
 		
 $consolids = array();
-
+foreach ($sources as $source){
 $i = 1;
-
 while($i < count($source) - 1){
 $consolids[] = $source[$i];
 $i++;
 } //end while i
-
 } //end foreach
-
 $bc = "/^\W*B\.?C\.?/i";
-
 $events = array();
-
 $bc_events = array();
-
 foreach ($consolids as $consolid){
-
 $split=array();
 	
 $textarr = str_split(strip_tags($consolid));
 	
 $has_tags = "/^</";
-
 if(! preg_match($has_tags, $textarr)){
 	while(preg_match("/\d/",$textarr[0])){
 	$leaddig = array_shift($textarr);
@@ -144,11 +112,8 @@ else
 {
 $events[] = array('year' => $split[0], 'event' => $split[1]);
 } //end if else BC
-
 } //end foreach consolids
-
 rsort($bc_events);
-
 sort($events);
 		
 echo "<ul>";
@@ -156,17 +121,11 @@ echo "<ul>";
 foreach ($bc_events as $event){
 echo "<li>" . $event['year'] . " BC: " . $event['event']  . "</li>";
 } //end foreach bc_events
-
    
 foreach ($events as $event){
 echo "<li>" . $event['year'] . ": " . $event['event']  . "</li>";
 } //end foreach events
 echo "</ul>";
-
 echo "<p><a href='quiz.php'>Take a general knowledge quiz</a></p>";
-
 ?>
-
-</body>
-
-</html>
+	</body></html>
